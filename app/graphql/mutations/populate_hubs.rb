b9508a8f15aaed1319834bd6ce621cfc575aa5d8
Alex Olson <alex.olson@gis-ag.com>
2019-04-11 23:17:25 +0200

    correct response from populate hubs.

--- app/graphql/mutations/populate_hubs.rb
+++ app/graphql/mutations/populate_hubs.rb
@@ -4,8 +4,15 @@ module Mutations
     field :errors, [String], null: false
 
     def resolve
+      return { 
+        success: false,
+        errors: ['Hubs present in database. Database must be empty in order for this mutation to succeed.']
+      } unless Hub.first.nil?
+
       hub_items = []
+
       locode_zip_file = open('https://www.unece.org/fileadmin/DAM/cefact/locode/loc182csv.zip')
+
       Zip::File.open_buffer(locode_zip_file) do |zip|
         zip.select{ |e| e.name.downcase.include? 'codelistpart' }.each do |entry|
           CSV.parse(entry.get_input_stream.read, headers: false) do |row|
@@ -27,7 +34,7 @@ module Mutations
               sub_div: row[5],
               function: row[6],
               status: row[7],
-              date: Date.strptime(row[8], '%y%m'),
+              date: parse_date(row[8]),
               iata: row[9],
               # TODO: Use Haversine formula/gem to get nearest distances
               coordinates:  lat_lng.nil? ? nil : ActiveRecord::Point.new(lat_lng[0], lat_lng[1]),
@@ -40,9 +47,15 @@ module Mutations
       Hub.import(hub_items)
 
       {
-        success: false,
-        errors: ['Not done yet']
+        success: true,
+        errors: []
       }
+    end
+
+    private
+
+    def parse_date(date_str)
+      Date.strptime(date_str, '%y%m') unless date_str.nil?
     end
   end
 end
