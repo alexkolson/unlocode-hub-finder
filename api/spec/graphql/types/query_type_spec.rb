90dd8d312fa57a3a5ece7f15072adc897502dd47
Alex Olson <alex.olson@gis-ag.com>
2019-04-15 00:25:25 +0200

    test invalid filter key condition for hubs query.

--- api/spec/graphql/types/query_type_spec.rb
+++ api/spec/graphql/types/query_type_spec.rb
@@ -34,6 +34,25 @@ describe Types::QueryType do
         expect(query_result['data']['hubs']['nextPage']).to be 2
       end
     end
-  end
 
+    context 'with invalid filter argument (invalid key)' do
+      it 'should return an error' do
+        query = <<-GRAPHQL
+          query {
+            hubs(filter: { key: "fake", value: "ham" }) {
+              page {
+                name
+              },
+            }
+          }
+        GRAPHQL
+
+        query_result = HubFinderApiSchema.execute query
+
+        expect(query_result['data']).to be_nil
+        expect(query_result['errors'].length).to be 1
+        expect(query_result['errors'].first['message']).to eq 'filter key (fake) must be an attribute on a Hub!'
+      end
+    end
+  end
 end
