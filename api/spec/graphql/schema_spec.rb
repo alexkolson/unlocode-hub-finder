dcb116ba612f58b53ed700cda7a692905e2a6f66
Alex Olson <alex.olson@gis-ag.com>
2019-04-14 22:28:48 +0200

    wording of schema spec test.

--- api/spec/graphql/schema_spec.rb
+++ api/spec/graphql/schema_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe HubFinderApiSchema do
-  it 'schema file should match actual current schema' do
+  it 'should match schema file' do
     current_defn = HubFinderApiSchema.to_definition
     printout_defn = File.read(Rails.root.join('app/graphql/schema.graphql'))
     expect(current_defn).to eq(printout_defn)
