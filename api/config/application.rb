12da79de5deb0ac93f17dcc5441bdff424dbfca9
Alex Olson <alex.olson@gis-ag.com>
2019-04-17 13:14:32 +0200

    initial query component structure.

--- api/config/application.rb
+++ api/config/application.rb
@@ -18,5 +18,12 @@ module HubFinderApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3001'
+        resource '*', headers: :any, methods: [:post, :options]
+      end
+    end
   end
 end
